c28e04e5d47aec38fbfe40b51e29764d9ef479fd
Acasune <tttt4aaaa@gmail.com>
2022-07-10 12:28:26 +0900

    add:dfs and bfs

---
+++ src/snippets/dfs_bfs.rs
@@ -0,0 +1,41 @@
+﻿fn bfs(s: usize, G: &Vec<Vec<usize>>) -> Vec<usize> {
+    // snippet begins
+    use std::collections::VecDeque;
+    let bfs_max = usize::MAX / 10;
+    let mut dists = vec![bfs_max; G.len()];
+    let mut que = VecDeque::new();
+    dists[s] = 0;
+    que.push_back(s);
+    while let Some(s) = que.pop_front() {
+        for &nxt in &G[s] {
+            if dists[nxt] == bfs_max {
+                dists[nxt] = dists[s] + 1;
+                que.push_back(nxt);
+            }
+        }
+    }
+
+    // snippet ends
+    dists
+}
+
+fn dfs(s: usize, G: &Vec<Vec<usize>>) -> Vec<usize> {
+    // snippet begins
+    use std::collections::VecDeque;
+    let bfs_max = usize::MAX / 10;
+    let mut dists = vec![bfs_max; G.len()];
+    let mut que = VecDeque::new();
+    dists[s] = 0;
+    que.push_back(s);
+    while let Some(s) = que.pop_back() {
+        for &nxt in &G[s] {
+            if dists[nxt] == bfs_max {
+                dists[nxt] = dists[s] + 1;
+                que.push_back(nxt);
+            }
+        }
+    }
+
+    // snippet ends
+    dists
+}
--- src/snippets/mod.rs
+++ src/snippets/mod.rs
@@ -1,2 +1,3 @@
-﻿pub mod dijkstra;
+﻿pub mod dfs_bfs;
+pub mod dijkstra;
 pub mod vectors_rotates;
